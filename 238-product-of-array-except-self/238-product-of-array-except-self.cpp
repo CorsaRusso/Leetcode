a6811a4ed513db9b3e5a68e357a56aeb56d9785e
Justin Gu <77459359+CorsaRusso@users.noreply.github.com>
2022-05-03 18:38:33 -0400

    Time: 43 ms (21.09%), Space: 24 MB (81.97%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -8,10 +8,11 @@ public:
 
 
         for (int i = 0; i < n; i++){
+            int num = n - i - 1;
             vect[i] *= left;
-            vect[n - i - 1] *= right;
+            vect[num] *= right;
             left *= nums[i];
-            right *= nums[n- i - 1];
+            right *= nums[num];
         }
         return vect;
     }
