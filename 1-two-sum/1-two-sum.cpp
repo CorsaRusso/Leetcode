d4c50f2c5ee6f9e2caae0a74f8812a76272a523c
Justin Gu <77459359+CorsaRusso@users.noreply.github.com>
2022-05-03 16:01:13 -0400

    Time: 409 ms (30.49%), Space: 10.2 MB (87.38%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,16 +3,16 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int end = nums.size() - 1;
         int start = 0;
+        vector<int> retval = {};
         while(start < end){
             for (int i = start + 1; i <= end; i++){
                 if(nums[start] + nums [i] == target){
-                    vector<int> retval = {start,i};
+                    retval = {start,i};
                     return retval;
                 }
             }
             start++;
         }
-        vector<int> retval = {};
         return retval; 
     }
 };
