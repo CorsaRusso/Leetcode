61971ab5205220c45c30ca27fdd249fd92ac518b
Justin Gu <77459359+CorsaRusso@users.noreply.github.com>
2022-05-03 17:46:22 -0400

    Time: 102 ms (78.32%), Space: 52.2 MB (34.32%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int,int> map;
-        for (int i = 0; i < nums.size(); i++){
-            if (map[nums[i]] > 0){
+        set<int> set;
+        for(int i = 0; i < nums.size(); i++){
+            if (set.count(nums[i]) == 1){
                 return true;
             }
-            map[nums[i]]++;
+            set.insert(nums[i]);
         }
         return false;
-        
     }
 };
