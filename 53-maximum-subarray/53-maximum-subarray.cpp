06cf7e69cbf64047fe47f8ce0e76f469ef98d419
Justin Gu <77459359+CorsaRusso@users.noreply.github.com>
2022-05-03 19:16:29 -0400

    Time: 139 ms (53.73%), Space: 67.7 MB (55.27%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -3,9 +3,7 @@ public:
     int maxSubArray(vector<int>& nums) {
         int sum1 = nums[0];
         int sum2 = nums[0];
-        if(nums.size() == 1){
-            return sum1;
-        }
+
         for(int i = 1; i < nums.size(); i++){
             sum1 += nums[i];
             sum1 = max(sum1, nums[i]);
