c63d7098d59718500eb09b51f1ce84453b3a18b7
Justin Gu <77459359+CorsaRusso@users.noreply.github.com>
2022-05-03 18:37:35 -0400

    Time: 40 ms (28.13%), Space: 24 MB (81.97%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int product = 1;
-        int count = 0;
-        for(int i = 0; i < nums.size(); i ++){
-            if (nums[i] == 0){
-                count++;
-            }
+        int n = nums.size();
+        int left = 1;
+        int right = 1;
+        vector<int> vect(n, 1);
+
+
+        for (int i = 0; i < n; i++){
+            vect[i] *= left;
+            vect[n - i - 1] *= right;
+            left *= nums[i];
+            right *= nums[n- i - 1];
         }
-        if(count > 1){
-            for(int i = 0; i < nums.size(); i ++){
-                nums[i] = 0;
-            }
-            return nums;
-        } else if (count == 1){
-            for(int i = 0; i < nums.size(); i ++){
-                if(nums[i] != 0){
-                    product *= nums[i];
-                }
-            }
-            for(int i = 0; i < nums.size(); i ++){
-                if(nums[i] != 0){
-                    nums[i] = 0;
-                } else {
-                    nums[i] = product;
-                }
-            }
-        } else {
-            for(int i = 0; i < nums.size(); i ++){
-                product *= nums[i];
-            }
-            for(int i = 0; i < nums.size(); i ++){
-                nums[i] = product / nums[i];
-            }
-        }
-        return nums;
+        return vect;
     }
 };
