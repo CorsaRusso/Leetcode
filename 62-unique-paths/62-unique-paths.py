5920532de756d73858df5811bcf487aa3b276dcc
Justin Gu <77459359+JusGu@users.noreply.github.com>
2022-06-26 21:28:07 -0400

    Time: 69 ms (8.11%), Space: 13.8 MB (73.25%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,4 +1,4 @@
-class Solution(object):
-    def uniquePaths(self, m, n):
-        return (math.factorial(m + n - 2)/(math.factorial(n - 1) * math.factorial(m - 1)))
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        return math.comb(m + n - 2, n - 1)
         
