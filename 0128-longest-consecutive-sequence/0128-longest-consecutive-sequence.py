dcf3815935daad446a9f10176624bb802fa413cf
Justin Gu <77459359+JusGu@users.noreply.github.com>
2023-02-20 14:50:20 -0500

    Time: 1984 ms (12.58%), Space: 30.7 MB (53.49%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -7,13 +7,13 @@ class Solution(object):
         nums_set = set(nums)
         max_length = 0
         for num in nums:
-            if (num - 1) in nums_set:
+            if num - 1 in nums_set:
                 continue
-            cur_length = 0
-            j = num
-            while j in nums_set:
+            now = num
+            cur_length = 1
+            while(now + 1 in nums_set):
+                now += 1
                 cur_length += 1
-                j += 1
             max_length = max(max_length, cur_length)
         return max_length
             
