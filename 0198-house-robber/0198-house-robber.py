53cd1819c2314433106cdd991cddf9277fb4604c
Justin Gu <77459359+JusGu@users.noreply.github.com>
2023-06-09 14:43:22 -0400

    Time: 25 ms (10.51%), Space: 13.4 MB (22.42%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # [1,2,3,5]
+        # [0,0,0,0]
+        max_money = [0 for i in nums]
+        global_max = 0
+        for i in range(len(max_money)):
+            prev_house = 0 if (i - 1) < 0 else max_money[i-1]
+            second_prev_house = 0 if (i - 2) < 0  else max_money[i-2]
+            max_money[i] = max(prev_house, second_prev_house + nums[i])
+            global_max = max(global_max, max_money[i])
+        return global_max
+            
